eb99c42309d292bafb7f6d84ca8c27068a2d9b7e
Ratatouille <cshimmaincontact@gmail.com>
2023-11-27 16:28:52 -0500

    font edit

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link 
+        rel="preload"
+        href="../assets/monument.otf"
+        as="font"
+        type="font/otf"
+        crossOrigin='anonymous'
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
