98e1e67fd69884307623b4dac84922e695108582
Ratatouille <cshimmaincontact@gmail.com>
2023-11-28 02:29:53 -0500

    edit font

--- components/HomePage/HomeHeader.js
+++ components/HomePage/HomeHeader.js
@@ -1,14 +1,13 @@
 import Script from "next/script";
-// import {Monument} from "@next/font/local"
-import localFont from "@next/font/local"
+
 
 export default function HomeHeader() {
 
-const monument = localFont({src:"../assets/monument.otf"})
+
 
   return (
     <section className="text-7xl mx-auto text-center w-full pt-[100px] pb-[76px] text-white">
-      <div className={monument.className}>
+      <div>
         <h1 className="leading-tight text-2xl md:text-[64px]">The New Pop,</h1>
         <h1 className="leading-tight text-2xl md:text-[64px]">
           <Script src="https://kit.fontawesome.com/4d06b44541.js" />
--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,19 @@ import "@/styles/globals.css";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 import { CartContextProvider } from "@/components/CartContext";
+import localFont from "@next/font/local"
+
+const monument = localFont({src:"../assets/monument.otf"})
 
 export default function App({ Component, pageProps }) {
   return (
     <>
     <CartContextProvider>
       <Nav />
+      <div className={monument.className}>
       <Component {...pageProps} />
+      </div>
+      
       <Footer/>
       </CartContextProvider>
     </>
