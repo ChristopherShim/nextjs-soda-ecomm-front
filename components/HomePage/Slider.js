63f5cb15cf7c57aac99c6706323c3a99338c4b74
Ratatouille <cshimmaincontact@gmail.com>
2023-11-29 03:10:56 -0500

    animations added

--- components/HomePage/Slider.js
+++ components/HomePage/Slider.js
@@ -1,7 +1,7 @@
-export default function Slider(){
-    return (
-        <section>
-          <div className="sliderGradient">
+export default function Slider() {
+  return (
+    <section>
+      {/* <div className="sliderGradient">
             <div className="h-[75px] t-0 l-0 w-full relative overflow-hidden">
               <ul className="relative inline-block list-none m-0 whitespace-nowrap w-[1540px] flex">
                 <li className="slider-li" >Strawberry Kiwi</li>
@@ -29,8 +29,29 @@ export default function Slider(){
                   <i className="fa-solid fa-circle" style={{ color: "#FFF" }}></i>
                 </li>
               </ul>
+              
             </div>
-          </div>
-        </section>
-      );
+          </div> */}
+
+      <div className="text-wrapper sliderGradient">
+        <div className="text-slider flex">
+        <p>strawberry kiwi</p>
+        <p>passion punch</p>
+        <p>lemon zest</p>
+        <p>strawberry kiwi</p>
+        <p>passion punch</p>
+        <p>lemon zest</p>
+        <p>strawberry kiwi</p>
+        <p>passion punch</p>
+        <p>lemon zest</p>
+        <p>strawberry kiwi</p>
+        <p>passion punch</p>
+        <p>lemon zest</p>
+        <p>strawberry kiwi</p>
+        <p>passion punch</p>
+        <p>lemon zest</p>
+        </div>
+      </div>
+    </section>
+  );
 }
